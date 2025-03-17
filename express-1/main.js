9e2de367036609f10cb498a1462c1f44d1e0cbbe
Basirat Hamayoon <basirathumayun@gmail.com>
2025-03-17 23:19:30 +0500

    Sending json data in routes also create some important steps in notes of the previous learning

--- express-1/main.js
+++ express-1/main.js
@@ -4,6 +4,14 @@ const port = 2000;
 app.get('/htmlData', (req, res) => {
     res.write('<h1>Sending Data through HtMl in Express js</h1>');
     res.send();
+});
+
+app.get('/', (req, res) => {
+    res.json({
+        id: 1,
+        name: "Joye",
+        age: 21
+    });
 })
 
 app.listen(port, () => {
