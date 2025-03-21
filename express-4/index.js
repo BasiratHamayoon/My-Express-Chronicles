3a838b15971ab3cac71eae92d4835b9770a1eac4
Basirat Hamayoon <basirathumayun@gmail.com>
2025-03-21 22:07:25 +0500

    Create a body-parser middleware to store the user data into mongo db also creste a mongo db connection in this project

--- express-4/index.js
+++ express-4/index.js
@@ -1,13 +1,46 @@
 const express = require('express');
 const app = express();
-const port = 6000;
-app.use((req, res, next) =>{
-    console.log("Middleware 1");
-    next();
-})
-app.use("/", (req, res) => {
-    res.send("Welcome to the Server!");
-})
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const port = 7000;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb://localhost:27017/UserData");
+        console.log("Mongo Db connected Sucessfully!");
+    } catch (error) {
+        console.log(`Mongo Db Connection error ${error}`);
+    }
+};
+connectDB();
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String
+});
+
+const User = mongoose.model("User", userSchema);
+
+
+app.set("view engine", "hbs");
+app.set("views", "./views")
+
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.post("/submit", async (req, res) => {
+    try {
+        const { name, email } = req.body;
+        const newUser = new User({ name, email });
+        await newUser.save();
+        res.send("User data  Saved Successfully!");
+    } catch (error) {
+        res.status(500).send("Error Saving Data")
+    }
+});
 
 app.listen(port, ()=> {
     console.log(`Server is running at http://localhost:${port}`);
